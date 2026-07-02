8000e147b06eaa958867409f6779711a222c8e84
Silvvz <yagosilva3014@gmail.com>
2026-07-02 09:37:50 -0300

    Criação do banco

---
+++ Backend/database/dbConnection.js
@@ -0,0 +1,40 @@
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize('Escola' , 'root', 'senai', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306,
+    logging: false
+});
+
+export async function setup() {
+    try {
+        const temp = new Sequelize('', 'root', 'senai', {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306,
+            logging: false
+
+        });
+        await temp.authenticate();
+
+        await temp.query(`
+            CREATE DATABASE IF NOT EXISTS Escola
+        `);
+
+        logger.info('Banco criado com sucesso!');
+
+        await temp.close();
+
+        await banco.authenticate();
+
+        logger.info('Conectado ao banco');
+
+        await banco.sync({ alter: true });
+
+        logger.info('Tabelas sincronizadas');
+    } catch (error) {
+        console.error(error);
+    }
+}
+    export default banco;
