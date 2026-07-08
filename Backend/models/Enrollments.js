dc5f2bb89c20dbffabc0aeda548c0549b5397f15
Silvvz <yagosilva3014@gmail.com>
2026-07-08 10:54:15 -0300

    Update Enrollment.js

--- Backend/models/Enrollments.js
+++ Backend/models/Enrollments.js
@@ -1,13 +1,22 @@
 import { DataTypes } from "sequelize";
 import banco from "../database/dbConnection.js";
 
-const Enrollment = banco.define('Enrollment', {
+const Enrollment = banco.define("Enrollment", {
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true
+    },
+
+    studentId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+
+    classId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
     }
 });
 
 export default Enrollment;
-
