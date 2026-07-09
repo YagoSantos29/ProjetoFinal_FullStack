29526c6cdf8cc2976f02b4751971b3f7c89ceda0
Silvvz <yagosilva3014@gmail.com>
2026-07-09 08:13:26 -0300

    Update userRoutes.js

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -12,7 +12,11 @@ const router = Router();
 router.post("/", authMiddleware, roleMiddleware("admin"), UserController.createUser);
 router.get("/", authMiddleware, roleMiddleware("admin"), UserController.getAllUsers);
 router.get("/:id", authMiddleware, roleMiddleware("admin"), UserController.getUserById);
+<<<<<<< Updated upstream
 router.put("/:id", authMiddleware, roleMiddleware("admin"), UserController.updateUser);
+=======
+router.put("/:id", authMiddleware, roleMiddleware("admin"), UserController.updateUserUser);
+>>>>>>> Stashed changes
 router.delete("/:id", authMiddleware, roleMiddleware("admin"), UserController.deleteUser);
 
 export default router;
