7782e24183ba10be3fa6ffec9668b2b0322d3c3a
Silvvz <yagosilva3014@gmail.com>
2026-07-23 10:48:31 -0300

    ARRUMEI CARALHO!!! obs: obgg cloude

--- front_end_FullStack/src/services/studentService.js
+++ front_end_FullStack/src/services/studentService.js
@@ -17,7 +17,7 @@ export const updateStudent = (id, data) => {
 };
 
 export const deleteStudent = (id) => {
-    return api.delete(`/student/${id}`);
+    return api.delete(`/students/${id}`);
 };
 
 export const viewStudent = () => {
